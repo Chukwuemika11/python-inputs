b66e5c8136aa048098bb0c3ce236d1e96ba09809
Chukwuemika11 <izunduchukwuemika9@gmail.com>
2024-09-26 23:17:22 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+print("welcome to my zempaa bank")
+name = input("Enter a name: ")
+account = int(input("Enter account number: "))
+password = (input("Enter password: "))
+age = int(input("Enter age: "))
+
+print("your account details")
+print(f" your name is {name} your account number is {account} your password is {password} and finally your age is {age}")
+
+print("you are welcome once again")
+balance = 0.00
+print(f"your account balance is ${balance}")
+
+
